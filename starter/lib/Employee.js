24a7a76a5f087af76bf3f46539ea41bbc3bd69ee
unknown <fpsingcak@gmail.com>
2023-03-03 15:04:57 +0000

    employee class done

--- starter/lib/Employee.js
+++ starter/lib/Employee.js
@@ -1,10 +1,47 @@
 // TODO: Write code to define and export the Employee class
-function Employee(){
-    constructor(name, email, id);
-        this.name = name;
-        this.email = email;
-        this.id = id;
+class Employee {
 
-};
+  constructor(name, id, email) {
+    
+    if (!name) {
+      throw new Error("Please provide name.");
+    };
+    if (!id) {
+      throw new Error("Please provide ID.");
+    };
+    if (!email) {
+      throw new Error("Please provide email.");
+    };
+
+    this.name = name;
+    this.id = id;
+    this.email = email;
+    this.role = 'Employee';
+  }
+
+    getName(){
+        console.log(this.name);
+    };
+    getId(){
+      console.log(this.id);
+    };
+    getEmail() {
+      console.log(this.email);
+    };
+    getRole(){
+      console.log(this.role);
+    };
+  };
 
 
+const e = new Employee('flor s','fps@fps',25);
+
+console.log(typeof(e));
+console.log(e.name);
+console.log(e.id);
+console.log(e.email);
+
+e.getEmail();
+e.getId();
+e.getName();
+e.getRole();
