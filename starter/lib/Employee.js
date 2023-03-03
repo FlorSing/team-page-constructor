e4f2113a1ba52bb8d24752452169ad4806bda2f2
FlorSing <fpsingcak@gmail.com>
2023-03-03 18:44:50 +0000

    updating files

--- starter/lib/Employee.js
+++ starter/lib/Employee.js
@@ -1,1 +1,15 @@
 // TODO: Write code to define and export the Employee class
+
+function Employee(name, id, email) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+};
+
+Employee.prototype.getName() = function(){
+    return empName = this.name;
+}
+
+
+
+module.exports = Employee;
