39fc43bcd90a6a5216568e98cc4d42090dcf9d8e
FlorSing <fpsingcak@gmail.com>
2023-03-07 23:16:06 +0000

    finished - cleanup done

--- starter/index.js
+++ starter/index.js
@@ -41,7 +41,7 @@ const builder = [
     type: 'list',
     name: 'direction',
     message: 'What would you like to do?',
-    choices: ['Add an Engineer', 'Add an Intern', 'Finish building the team'],
+    choices: ['Add an Engineer', 'Add an Intern', 'Finish building team'],
     filter(val){
       return add = val;
     }    
@@ -97,54 +97,48 @@ const intern = [
 
 const html = [];
 
+
 function buildStart() {
     inquirer.prompt(manager).then((answers) =>{
-      render(new Manager(answers.managerName, answers.managerID, answers.managerEmail, answers.officeNumber));
-      // html.push(e);
-      // console.log(html);
-      // fs.writeFile(__dirname, "output", `${answer.managerName}.json`,JSON.stringify(answers, null, '  ') )
+      const manager = new Manager(answers.managerName, answers.managerID, answers.managerEmail, answers.officeNumber);
       buildQuestions();
-    })    
+      html.push(manager);
+    })
+         
 }      
 
 function buildQuestions() {
     inquirer.prompt(builder).then((answers) => {
-    buildTeam();
-    return answers;
+      buildTeam();
+      // return answers;
     })
+    
 }
 
 function buildTeam() {
-    if (add === 'Finish building team'){
-      console.log(JSON.stringify(answers, null, '  '))
-  }
     if (add === 'Add an Engineer') {
-         inquirer.prompt(engineer).then((answers) => {
-          render(new Engineer(answers.engineerName, answers.engineerID, answers.engineerEmail, answers.github));
-          // html.push(e);
-          // console.log(html);
-         buildQuestions();
+        inquirer.prompt(engineer).then((answers) => {
+        const engineer = new Engineer(answers.engineerName, answers.engineerID, answers.engineerEmail, answers.githubID);
+        buildQuestions();
+        html.push(engineer);
+        console.log(engineer.getRole());
+
          })
     }
+
     if (add === 'Add an Intern') {
-     inquirer.prompt(intern).then((answers) => {
-          render(new Intern(answers.internName, answers.internID, answers.internEmail, answers.school));
-          // html.push(e);
-          // console.log(html);
-         buildQuestions();
-     })
+        inquirer.prompt(intern).then((answers) => {
+        const intern = new Intern(answers.internName, answers.internID, answers.internEmail, answers.school);
+        buildQuestions();
+        html.push(intern);
+        })
     }
+
+    if (add === 'Finish building team'){
+        fs.writeFile(outputPath, render(html), (err) => err? console.error(err) : console.log('..created team.html file'));
+      };
   }
 
-// console.log[html];
 buildStart();
 
-// const engr = new Engineer(`${answer.name}`, `${answer.empID}`, `${answers.empEmail}`, `${answers.github}`)
 
-// inquirer.prompt(questions)
-//             .then((answers) => fs.writeFile(outputPath, render(answers)))
-//             .then((answers) => console.log(JSON.stringify(answers, null, '  ')))
-//             .then(() => console.log('Successfully wrote to team.html'))
-//             .catch((err) => console.error(err));
-
-          
--- starter/testwritetooutput.js
+++
@@ -1,4 +0,0 @@
-
-const fs = require("fs");
-
-fs.writeFile('../output/trytext.txt', 'testing write file to output folder');
